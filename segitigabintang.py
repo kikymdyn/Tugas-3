8fcf59902b98a31653cca6952600c337c158a58c
Muhammad Ribqi Madian Putra <rifkymadianp@gmail.com>
2024-09-22 01:52:02 +0700

    Create segitigabintang.py

---
+++ segitigabintang.py
@@ -0,0 +1,22 @@
+def print_pattern(n):
+    for i in range(n):
+        print(" " * (i * 2) + "* " * (n - i))
+
+n = 8
+print_pattern(n)
+
+
+
+
+
+
+PS C:\Users\LENOVO> & C:/Users/LENOVO/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/LENOVO/Pictures/Media Madian/Madian/segitigabintang.py"
+* * * * * * * * 
+  * * * * * * *
+    * * * * * *
+      * * * * *
+        * * * *
+          * * *
+            * *
+              *
+PS C:\Users\LENOVO>
