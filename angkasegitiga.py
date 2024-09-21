2ac63c4986b11240d4b458a04dde534ebac05e3e
Muhammad Ribqi Madian Putra <rifkymadianp@gmail.com>
2024-09-22 01:51:08 +0700

    Create angkasegitiga.py

---
+++ angkasegitiga.py
@@ -0,0 +1,23 @@
+def print_number_pattern(n):
+    for i in range(n, 0, -1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+n = 7
+print_number_pattern(n)
+
+
+
+
+
+
+PS C:\Users\LENOVO> & C:/Users/LENOVO/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/LENOVO/Pictures/Media Madian/Madian/angkasegitiga.py"
+1 2 3 4 5 6 7 
+1 2 3 4 5 6 
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2
+1
+PS C:\Users\LENOVO> 
